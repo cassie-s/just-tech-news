dfce0c0fde6e4705c8ad86531fa6be0b30fe0b53
cassie-s <56046933+cassie-s@users.noreply.github.com>
2021-11-11 19:42:44 -0600

    fix connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,8 @@
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+// create connection to our db
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -9,3 +14,5 @@ if (process.env.JAWSDB_URL) {
     port: 3306
   });
 }
+
+module.exports = sequelize;
